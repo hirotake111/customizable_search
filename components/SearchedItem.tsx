9cbbea54b97c779552c52b9cf44680db98824de5
hirotake111 <hirotake111@users.noreply.github.com>
2022-01-29 06:29:02 +0000

    fized Link component issue

--- components/SearchedItem.tsx
+++ components/SearchedItem.tsx
@@ -9,16 +9,20 @@ interface Props {
 export default function SearchedItem({ title, link, snippet }: Props) {
   return (
     <div className="flex flex-col mb-5">
-      <Link href={link} passHref>
-        <a target="_blank">
-          <div aria-label="url" className="mb-1">
-            <span className="text-sm text-gray-800">{link}</span>
-          </div>
-          <div aria-label="title" className="mb-1">
-            <span className="text-xl text-blue-800">{title}</span>
-          </div>
-        </a>
-      </Link>
+      <div aria-label="url" className="mb-1">
+        <span className="text-sm text-gray-800">
+          <Link href={link} passHref>
+            <a target="_blank">{link}</a>
+          </Link>
+        </span>
+      </div>
+      <div aria-label="title" className="mb-1">
+        <span className="text-xl text-blue-800">
+          <Link href={link} passHref>
+            <a target="_blank">{title}</a>
+          </Link>
+        </span>
+      </div>
       <div aria-label="snippet">
         <span className="text-sm text-gray-600">{snippet}</span>
       </div>
