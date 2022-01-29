30633462afeb3caf01421528151f4fcbd7389e33
hirotake111 <hirotake111@users.noreply.github.com>
2022-01-29 05:58:41 +0000

    add verbose logs more

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -27,12 +27,12 @@ export default async function searchHandler(
       return;
     }
     // fetch data from Google custom search API
-    const { data, status } = await axios.get(url);
+    const { data, status } = await axios.get(url, {});
     res.status(200).send({ result: "success", data });
   } catch (e) {
     if (e instanceof Error) {
       res.status(500).send({
-        result: e.message,
+        result: e,
       });
       return;
     }
